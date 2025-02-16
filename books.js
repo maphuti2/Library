fe9089745e22c2e84787c58b5b0ac41e86a24edc
maphuti2 <maphuti22r@gmail.com>
2025-02-17 00:52:59 +0200

    function to add book

--- books.js
+++ books.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const pages = document.querySelector('#pages');
     const status = document.querySelector('#read');
     const newBook = document.querySelector('#new-book');
+    const bookCover = document.querySelector('.book-cover')
 
 
     //main things
@@ -26,7 +27,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const bookTitle = title.value;
         const bookAuthor = author.value;
         const bookPage = pages.value;
-        const bookStatus = status.value;
+        const bookStatus = status.checked ? 'Read' : 'Not Read';
 
         const book = new Book(bookTitle, bookAuthor, bookPage,bookStatus);
 
@@ -35,24 +36,53 @@ document.addEventListener('DOMContentLoaded', ()=>{
         dialog.close();
     }
 
+    // Functions to do the work
+
+    function checkBookAvailability(){
+        if (bookCover.children.length === 0){
+            document.querySelector('.book-avail').style.display = 'block';
+        }
+        else{
+            document.querySelector('.book-avail').style.display = 'none';
+        }
+    }
+
+    function displayBook(){
+        myLibrary.forEach((book, index) =>{
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('book');
+            bookDiv.innerHTML = `
+            <div class="book-design">
+                <p class="book-title">${book.title}</p>
+                <p class="book-author"><span>&bullet;</span>${book.author}</p>
+                <p class="book-pages">${book.pages}</p>
+                <p class="book-read">${book.status}</p>
+                <div class="book-btn">
+                    <button class="status" data-index="${index}">${book.status}</button>
+                    <button class="delete" data-index="${index}">Delete</button>
+                </div>
+            </div>
+            `;
+            bookCover.appendChild(bookDiv);
+        });
+
+        readStatus.forEach((btn) => {
+            btn.addEventListener('click', changeStatus);
+        });
+
+        deleteBtn.forEach((del) =>{
+            del.addEventListener('click', removeBook);
+        });
+    }
+
     addBook.addEventListener('click', () =>{
         dialog.show();
     });
 
+    checkBookAvailability();
+
     closeBtn.addEventListener('click', () =>{
         dialog.close();
-    });
-
-    readStatus.forEach(read => {
-        read.addEventListener('click', () =>{
-            alert('read read...');
-        });
-    });
-    
-    deleteBtn.forEach(del => {
-        del.addEventListener('click', () =>{
-            alert('Delete delete...');
-        });
     });
     
 });
